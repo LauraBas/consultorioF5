652dfb72ada7f472b0ad20431f8ff3adf8cd3ac5
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-22 14:39:45 +0100

    change to sql file

--- src/Models/citas.sql
+++ src/Models/citas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 11, 2020 at 10:31 AM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+-- Generation Time: Nov 22, 2020 at 01:29 PM
+-- Server version: 5.7.30
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -24,19 +24,19 @@ CREATE TABLE `agenda` (
   `ID` int(11) NOT NULL,
   `Coder/Team` varchar(15) NOT NULL,
   `Topic` varchar(15) NOT NULL,
-  `Date/Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Date/Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Description` text NOT NULL,
-  `pendiente` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pendiente` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `agenda`
 --
 
 INSERT INTO `agenda` (`ID`, `Coder/Team`, `Topic`, `Date/Time`, `Description`, `pendiente`) VALUES
-(3, 'atom', 'php', '2020-11-10 11:25:43', 'help', NULL),
-(6, 'paula', 'aka', '2020-11-10 12:10:43', 'nuevo', NULL),
-(7, 'test nuevo', 'nuevo', '2020-11-10 13:14:04', 'funciona', NULL);
+(1, 'pepe', 'duda', '2020-11-22 12:29:15', 'no me sale', 0),
+(2, 'pepa', 'duda', '2020-11-22 12:29:16', 'no me sale', 1),
+(3, 'pipe', 'duda', '2020-11-22 12:29:16', 'no me sale', 1);
 
 --
 -- Indexes for dumped tables
@@ -56,4 +56,4 @@ ALTER TABLE `agenda`
 -- AUTO_INCREMENT for table `agenda`
 --
 ALTER TABLE `agenda`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
